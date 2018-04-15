2e9137714ff0e4e72211a929eaec0e9331847ec8
indrek_k <indrek@codelight.eu>
2018-04-15 19:34:47 +0000

    Set required version back. Add support for situations where email type field is not present.

---
+++ readme.txt
@@ -0,0 +1,39 @@
+
+=== Gravity Forms: GDPR Add-On ===
+Contributors: codelight
+Tags: gdpr, gravity forms
+Requires at least: 4.7
+Tested up to: 4.9.4
+Requires PHP: 5.6.33
+Stable tag: 1.0.0
+License: GPLv3
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+
+The easiest way to make your Gravity Forms GDPR-compliant. Fully documented, extendable and developer-friendly.
+
+== Description ==
+
+The easiest way to make your Gravity Forms GDPR-compliant GDPR compliant!
+
+This plugin adds new privacy features to Gravity Forms. Your visitors can download or delete their form submissions automatically or submit a request for the site admin to do so.
+
+Until WordPress releases their own GDPR compliance update, this plugin requires [The GDPR Framework](https://wordpress.org/plugins/gdpr-framework/) to function (it's free!)
+
+Make sure to also read the guide! You don't need to drown your customers in pointless acceptance checkboxes if you know what you're doing!
+
+## Disclaimer
+Using Gravity Forms: GDPR Add-On does NOT guarantee compliance to GDPR. This plugin gives you general information and tools, but is NOT meant to serve as complete compliance package. Compliance to GDPR is risk-based ongoing process that involves your whole business. Codelight is not eligible for any claim or action based on any information or functionality provided by this plugin.
+
+### Documentation
+How to use this plugin (practical guide): [Making your Gravity Forms GDPR-compliant](https://codelight.eu/wordpress-gdpr-framework/guide/making-your-forms-compliant/)
+How to use this plugin (the legal stuff explained): [Legal grounds for processing data](https://codelight.eu/wordpress-gdpr-framework/guide/legal-grounds-for-processing-data/)
+Full documentation: [The WordPress Site Owner's Guide to GDPR](https://codelight.eu/wordpress-gdpr-framework/wordpress-site-owners-guide-to-gdpr/)
+For developers: [Developer Docs](https://codelight.eu/wordpress-gdpr-framework/developer-docs/)
+
+### Features
+&#9745; Allow both users and visitors without an account to view, export and delete their form submissions or request the site admin to do so;
+&#9745; Configure forms to be excluded from viewing, exporting or deleting.
+
+### Coming soon
+&#9744; Support for anonymization: allow admin to select which fields must be anonymized;
+&#9744; Track, manage and withdraw consent.
--- src/GravityFormsGDPRAddOn.php
+++ src/GravityFormsGDPRAddOn.php
@@ -7,7 +7,7 @@ namespace Codelight\GDPR\Modules\GravityForms;
 class GravityFormsGDPRAddOn extends \GFAddOn
 {
     protected $_version = GF_GDPR_VERSION;
-    protected $_min_gravityforms_version = '2.2.6.5';
+    protected $_min_gravityforms_version = '2.1';
     protected $_slug = 'gdpr';
     protected $_path = 'gdpr-framework/src/Modules/GravityFormsGDPRAddOn.php';
     protected $_full_path = __FILE__;
@@ -41,37 +41,46 @@ class GravityFormsGDPRAddOn extends \GFAddOn
             }
         }
 
+        // Handle situations where the email field type is not 'email'
+        if (empty($emailFields)) {
+            foreach ($form['fields'] as $i => $field) {
+                /* @var $field \GF_Field */
+                $emailFields[$i]['label'] = $field->adminLabel ? $field->adminLabel : $field->label;
+                $emailFields[$i]['value'] = $field->id;
+            }
+        }
+
         return [
             [
-                'title'  => esc_html__('Privacy Settings', 'gdpr-admin'),
+                'title'  => __('Privacy Settings', 'gdpr-admin'),
                 'fields' => [
                     [
-                        'label'   => esc_html__('Email address field', 'gdpr-admin'),
+                        'label'   => __('Primary email address field', 'gdpr-admin'),
                         'type'    => 'select',
                         'name'    => 'gdpr_email_field',
-                        'tooltip' => esc_html__("Select the field which will contain the customer's primary email address. This is used to identify which form submission belongs to which customer.", 'gdpr-admin'),
+                        'tooltip' => __("Select the field which will contain the customer's primary email address. This is used to identify which form submission belongs to which customer.", 'gdpr-admin'),
                         'choices' => $emailFields,
                     ],
                     [
-                        'label'   => esc_html__('Exclude this form from automatic data download?', 'gdpr-admin'),
+                        'label'   => __('Exclude this form from automatic data download', 'gdpr-admin'),
                         'type'    => 'checkbox',
                         'name'    => 'gdpr_exclude_from_export',
-                        'tooltip' => esc_html__("Check this if you want this form to be excluded when a customer's data is downloaded automatically.", 'gdpr-admin'),
+                        'tooltip' => __("Check this if you want this form to be excluded when a customer's data is downloaded automatically.", 'gdpr-admin'),
                         'choices' => [
                             [
-                                'label' => esc_html__('Enabled', 'gdpr-admin'),
+                                'label' => __('Exclude', 'gdpr-admin'),
                                 'name'  => 'gdpr_exclude_from_export',
                             ],
                         ],
                     ],
                     [
-                        'label'   => esc_html__('Exclude this form from automatic data deletion?', 'gdpr-admin'),
+                        'label'   => __('Exclude this form from automatic data deletion', 'gdpr-admin'),
                         'type'    => 'checkbox',
                         'name'    => 'gdpr_exclude_from_delete',
-                        'tooltip' => esc_html__("Check this if you want this form to be excluded when a customer's data is deleted automatically.", 'gdpr-admin'),
+                        'tooltip' => __("Check this if you want this form to be excluded when a customer's data is deleted automatically.", 'gdpr-admin'),
                         'choices' => [
                             [
-                                'label' => esc_html__('Enabled', 'gdpr-admin'),
+                                'label' => __('Exclude', 'gdpr-admin'),
                                 'name'  => 'gdpr_exclude_from_delete',
                             ],
                         ],
